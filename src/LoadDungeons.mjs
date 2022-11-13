50ef9621bf7ccdfcbafab74ab48682c238a4112f
Dorky106 <greasontim@gmail.com>
2022-11-13 18:42:28 -0500

    0.0.6rc2
    Pushing second release candidate for 0.0.6

--- src/LoadDungeons.mjs
+++ src/LoadDungeons.mjs
@@ -1,3 +1,4 @@
 export async function Load(gameData) {
+    await gameData.addPackage("json/Dungeons/Dragonkin_Laboratory.json");
     console.log("Dungeons Loaded!");
 }
--- src/LoadSlayerAreas.mjs
+++ src/LoadSlayerAreas.mjs
@@ -1,6 +1,5 @@
 export async function Load(gameData) {
     await gameData.addPackage("json/Dungeons/The_Blood_Pact.json");
-    await gameData.addPackage("json/Dungeons/Dragonkin_Laboratory.json");
     await gameData.addPackage("json/Dungeons/Mole_Lair.json");
     await gameData.addPackage("json/Dungeons/Royal_Dragon_Lair.json");
     await gameData.addPackage("json/Dungeons/Temple_of_Aminishi.json");
